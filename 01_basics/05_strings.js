a19c35cdb3c8ef19209d154d9c7f097476859166
mohib080 <mohibul.sawrav2004@gmail.com>
2025-05-13 20:07:57 +0600

    Update 05_strings.js

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,9 +1,9 @@
 const name = "hitesh"
 const repoCount = 50
 
-// console.log(name + repoCount + " Value");
+// console.log(name + repoCount + " Value"); //bad practice
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);// string interpolation, backticks
 
 const gameName = new String('hitesh-hc-com')
 
@@ -17,19 +17,19 @@ console.log(gameName.charAt(2));
 console.log(gameName.indexOf('t'));
 
 const newString = gameName.substring(0, 4)
-console.log(newString);
+console.log(newString); // hite
 
 const anotherString = gameName.slice(-8, 4)
-console.log(anotherString);
+console.log(anotherString); // -hc-com
 
 const newStringOne = "   hitesh    "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+console.log(newStringOne); // "   hitesh    "
+console.log(newStringOne.trim()); // "hitesh"
 
 const url = "https://hitesh.com/hitesh%20choudhary"
 
-console.log(url.replace('%20', '-'))
+console.log(url.replace('%20', '-')) // https://hitesh.com/hitesh-choudhary
 
-console.log(url.includes('sundar'))
+console.log(url.includes('sundar')) // false
 
-console.log(gameName.split('-'));
+console.log(gameName.split('-')); // [ 'hitesh', 'hc', 'com' ]
