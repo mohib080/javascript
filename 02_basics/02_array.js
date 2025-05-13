e23bae52b6892e1ae88c231c43d72cd614890576
mohib080 <mohibul.sawrav2004@gmail.com>
2025-05-14 04:29:29 +0600

    Update 02_array.js

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -3,10 +3,10 @@ const dc_heros = ["superman", "flash", "batman"]
 
 // marvel_heros.push(dc_heros)
 
-// console.log(marvel_heros);
-// console.log(marvel_heros[3][1]);
+// console.log(marvel_heros); -- this will add the dc_heros array to the marvel_heros array(Array inside an array)
+// console.log(marvel_heros[3][1]); 
 
-// const allHeros = marvel_heros.concat(dc_heros)
+// const allHeros = marvel_heros.concat(dc_heros) -- this will combine the two arrays into one array(flattening the array)
 // console.log(allHeros);
 
 const all_new_heros = [...marvel_heros, ...dc_heros]
@@ -15,17 +15,17 @@ const all_new_heros = [...marvel_heros, ...dc_heros]
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
-const real_another_array = another_array.flat(Infinity)
-console.log(real_another_array);
+const real_another_array = another_array.flat(Infinity) // this will flatten the array to any level of depth
+console.log(real_another_array); // [1, 2, 3, 4, 5, 6, 7, 6, 7, 4, 5]
 
 
 
 console.log(Array.isArray("Hitesh"))
 console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+console.log(Array.from({name: "hitesh"})) // interesting -- this will create an array of the object keys
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3));
+console.log(Array.of(score1, score2, score3)); // this will create an array of the scores
