ba3c3ea77f46a975201f7a03260f512c06519b41
mohib080 <mohibul.sawrav2004@gmail.com>
2025-05-13 20:29:25 +0600

    Update 06_nums_and_math.js

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,15 +1,15 @@
 const score = 400
-// console.log(score);
+// console.log(score); // console.log(typeof score); // number
 
 const balance = new Number(100)
-// console.log(balance);
+// console.log(balance); // console.log(typeof balance); // object
 
 // console.log(balance.toString().length);
-// console.log(balance.toFixed(1));
+// console.log(balance.toFixed(1)); // console.log(balance.toFixed(2)); // 100.00
 
 const otherNumber = 123.8966
 
-// console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toPrecision(4)); // 123.9
 
 const hundreds = 1000000
 // console.log(hundreds.toLocaleString('en-IN'));
@@ -25,10 +25,10 @@ const hundreds = 1000000
 // console.log(Math.max(4, 3, 6, 8));
 
 console.log(Math.random());
-console.log((Math.random()*10) + 1);
-console.log(Math.floor(Math.random()*10) + 1);
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
 
 const min = 10
 const max = 20
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.floor(Math.random() * (max - min + 1)) + min) // 10 - 20 
