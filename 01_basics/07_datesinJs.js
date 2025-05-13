ec589adcfa39c32e52b615719e04cd25428f9f3e
mohib080 <mohibul.sawrav2004@gmail.com>
2025-05-14 04:13:36 +0600

    Update 07_datesinJs.js

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -1,10 +1,10 @@
 // Dates
 
 let myDate = new Date()
-// console.log(myDate.toString());
+// console.log(myDate.toString()); -
 // console.log(myDate.toDateString());
-// console.log(myDate.toLocaleString());
-// console.log(typeof myDate);
+// console.log(myDate.toLocaleString());  s
+// console.log(typeof myDate); ---- Object
 
 // let myCreatedDate = new Date(2023, 0, 23)
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
@@ -27,6 +27,6 @@ console.log(newDate.getDay());
 
 newDate.toLocaleString('default', {
     weekday: "long",
-    
+
 })
 
