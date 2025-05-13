4a9df537260a9f4dcd5accc5e15a95776c25ac71
mohib080 <mohibul.sawrav2004@gmail.com>
2025-05-14 04:23:01 +0600

    Update 01_arrays.js

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -12,13 +12,18 @@ const myArr2 = new Array(1, 2, 3, 4)
 // myArr.push(7)
 // myArr.pop()
 
-// myArr.unshift(9)
+// myArr.unshift(9) -- adds to the beginning of the array
 // myArr.shift()
 
 // console.log(myArr.includes(9));
 // console.log(myArr.indexOf(3));
 
-// const newArr = myArr.join()
+// const newArr = myArr.join() -- converts to string
+// const newArr = myArr.join(" - ") -- converts to string with separator
+// const newArr = myArr.concat(myHeors) -- combines two arrays
+// const newArr = myArr.concat(myHeors, 1, 2, 3) -- combines two arrays with additional values
+// const newArr = myArr.concat(myHeors, [1, 2, 3]) -- combines two arrays with additional array
+// const newArr = myArr.concat(myHeors, [1, 2, 3], 4) -- combines two arrays with additional array and value
 
 // console.log(myArr);
 // console.log( newArr);
@@ -26,14 +31,13 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 // slice, splice
 
-console.log("A ", myArr);
+console.log("A ", myArr); 
 
 const myn1 = myArr.slice(1, 3)
 
-console.log(myn1);
-console.log("B ", myArr);
+console.log(myn1); // slice does not change the original array --- [1, 2]
+console.log("B ", myArr); // original array is unchanged
 
-
-const myn2 = myArr.splice(1, 3)
+const myn2 = myArr.splice(1, 3) // splice changes the original array
 console.log("C ", myArr);
-console.log(myn2);
+console.log(myn2); // splice returns the removed elements-- [1, 2, 3]
