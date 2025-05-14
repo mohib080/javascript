a8489fac8379de95afb2d18768e0c4e01466d25f
mohib080 <mohibul.sawrav2004@gmail.com>
2025-05-14 13:46:32 +0600

    Update 03_objects.js

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -6,10 +6,10 @@
 const mySym = Symbol("key1")
 
 
-const JsUser = {
+const JsUser = { // object literal
     name: "Hitesh",
     "full name": "Hitesh Choudhary",
-    [mySym]: "mykey1",
+    [mySym]: "mykey1", // this is a symbol key
     age: 18,
     location: "Jaipur",
     email: "hitesh@google.com",
@@ -17,13 +17,13 @@ const JsUser = {
     lastLoginDays: ["Monday", "Saturday"]
 }
 
-// console.log(JsUser.email)
-// console.log(JsUser["email"])
+// console.log(JsUser.email) --- this will print the email
+// console.log(JsUser["email"]) --- this will also print the email --- this is useful when the key has a space in it
 // console.log(JsUser["full name"])
-// console.log(JsUser[mySym])
+// console.log(JsUser[mySym]) --- this will print the value of the symbol key
 
 JsUser.email = "hitesh@chatgpt.com"
-// Object.freeze(JsUser)
+// Object.freeze(JsUser) -- this will make the object immutable
 JsUser.email = "hitesh@microsoft.com"
 // console.log(JsUser);
 
